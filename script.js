4d91d4d31542e2cefe95357071b4f3761c7fef50
SchmidtRichard <ri.schmidta@gmail.com>
2021-06-08 15:26:19 +0100

    finish and improve animations, pin first part of the page - end result

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ we can chain together multiple animations, basically create a timeline for the d
 */
 let timeline = new TimelineMax();
 
-timeline.to(".rock", 3, { y: -200 }).to(".panda", 3, { y: -200 }, "-=3").to(".bg1", 3, { y: 50 }, "-=3");
+timeline
+	.to(".rock", 3, { y: -200 })
+	.to(".panda", 3, { y: -200 }, "-=3")
+	.fromTo(".bg1", { y: -50 }, { y: 0, duration: 3 }, "-=3")
+	.to(".content", 3, { top: "0%" }, "-=3")
+	.fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 });
 
 /*the first parameter is the element, the second one is the duration, and the third what we want to do
 
@@ -19,3 +24,16 @@ The commented out codes below is just an example of what we can do with it
 // timeline.to(".text", 2, { x: 500 }).to(".content-images", 2, { opacity: 0 }, "-=2");
 
 // timeline.fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+//Set the animations to work only when we scroll the page
+let scene = new ScrollMagic.Scene({
+	triggerElement: "section",
+	duration: "300%",
+	triggerHook: 0
+})
+	//hook the animation to the scene
+	.setTween(timeline)
+	//Pin the first part of the page (the panda e mountains) so the other part of the website can come on top of it when the page is scrolled
+	.setPin("section")
+	//then add it to the controller
+	.addTo(controller);
